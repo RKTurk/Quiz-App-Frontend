32ecac3965c9cfe1a39ab2fbbb2c35eea70bba0d
Rizwan Khan Turk <rixwankhan99@gmail.com>
2024-01-04 12:13:15 +0500

    Login page updates

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
-const Login = ({ onLogin }) => {
+const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
 
-  const handleLogin = async () => {
+  const handleLogin = async (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+      // Check if username and password are not empty
+      if (!username || !password) {
+        toast.warning('Please enter both username and password!', {
+          position: 'top-right',
+          autoClose: 3000, // 3 seconds
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+        return; // Don't proceed with the login if fields are empty
+      }
     // Implement API call to the backend for authentication
     const response = await fetch('http://localhost:8080/api/login', {
       method: 'POST',
@@ -22,18 +37,35 @@ const Login = ({ onLogin }) => {
     const data = await response.json();
 
     if (data.token === 'user_found') {
-      alert("Login Successful"); // Assuming the backend sends a token upon successful login
-      navigate('/dashboard'); // Replace '/dashboard' with your desired route
-    } else {
-      alert("Invalid Credentials"); 
+      toast.success('Login Successful!', {
+        position: 'top-right',
+        autoClose: 3000, // 3 seconds
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+      navigate('/dashboard'); 
+    } else { 
+      toast.error('Invalid Credentials!', {
+        position: 'top-right',
+        autoClose: 3000, // 3 seconds
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+      setUsername('');
+      setPassword('');
     }
   };
 
   return (
     <div className='modal-content'>
     <form>
-      <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <h1>Login</h1>
+      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
       <button onClick={handleLogin}>Login</button>
     </form> 
     </div>
